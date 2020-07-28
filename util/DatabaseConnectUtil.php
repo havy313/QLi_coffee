c50109d624af7d53b0a02da45f36ccf1813963c9
Vy Trinh <50666715+havy313@users.noreply.github.com>
2020-07-28 22:04:04 +0700

    resolve conflict file DB

--- util/DatabaseConnectUtil.php
+++ util/DatabaseConnectUtil.php
@@ -1,13 +1,8 @@
 <?php
  	$localhost = 'localhost';
  	$username = 'root';
-<<<<<<< HEAD
  	$password = '12345';
- 	$database = 'lalalalala';
-=======
- 	$password = '308trungthuc';
  	$database = 'starbucks';
->>>>>>> 0a5496bcbba729f989eda2f21d687cd724abf61c
 
  	$mysqli = new mysqli($localhost, $username, $password, $database);
  	$mysqli->set_charset('utf8');
